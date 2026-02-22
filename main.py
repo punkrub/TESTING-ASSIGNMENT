ce3febd6b4fd248ddf1b13cb534c60e1ecb68304
Pun Suwannasang <punpunkapaipai@gmail.com>
2026-02-22 12:18:01 +0700

    add alternate

--- main.py
+++ main.py
@@ -23,3 +23,19 @@ def caesarCipher(s, k):
         else:
             result.append(char)
     return "".join(result)
+
+def alternate(s):
+    max_len = 0
+    unique_chars = list(set(s))
+    for i in range(len(unique_chars)):
+        for j in range(i + 1, len(unique_chars)):
+            char1, char2 = unique_chars[i], unique_chars[j]
+            filtered_s = [c for c in s if c == char1 or c == char2]
+            is_valid = True
+            for k in range(1, len(filtered_s)):
+                if filtered_s[k] == filtered_s[k-1]:
+                    is_valid = False
+                    break
+            if is_valid:
+                max_len = max(max_len, len(filtered_s))
+    return max_len

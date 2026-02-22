14b73ca83946a90958c08b4e3a5a4c2ffa30c1db
Pun Suwannasang <punpunkapaipai@gmail.com>
2026-02-22 12:17:17 +0700

    add caesarCipher

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ def alternatingCharacters(s):
         if s[i] == s[i-1]:
             deletions += 1
     return deletions
+
+def caesarCipher(s, k):
+    k = k % 26
+    result = []
+    for char in s:
+        if char.islower():
+            result.append(chr((ord(char) - 97 + k) % 26 + 97))
+        elif char.isupper():
+            result.append(chr((ord(char) - 65 + k) % 26 + 65))
+        else:
+            result.append(char)
+    return "".join(result)
